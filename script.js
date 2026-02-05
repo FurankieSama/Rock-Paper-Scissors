75e1a5827df533779281f76a02f65379ff075091
furankiesama <franknwokoma1@gmail.com>
2026-02-06 00:55:53 +0100

    Add: getHUmanChoice function
    collect human input using function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// etComputerChoice: Chooses between three options 
+// GetComputerChoice: Chooses between three options for computer
 // Rock Paper, and Scissors
 function getComputerChoice() {
     // F*ck it, I'm using an array because it's more efficient
@@ -8,3 +8,10 @@ function getComputerChoice() {
     // Return random array value
     return (gameOptions[Math.floor((Math.random() * (gameOptions.length )))])
 }
+
+// getHumanChoice: collect user input
+function getHumanChoice() {
+    let humanChoice = prompt("Enter a choice: ").toLowerCase()
+    return humanChoice;
+}
+
