3e0aa1e3c6f5fa425754f2e70f942fc79202f77e
furankiesama <franknwokoma1@gmail.com>
2026-02-07 14:47:06 +0100

    fix: playRound function
    fix logic in playRound function to allow ties and ensure the right input is given

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
 // GetComputerChoice: Chooses between three options for computer
 // Rock Paper, and Scissors
 function getComputerChoice() {
-    // F*ck it, I'm using an array because it's more efficient
-    // Declare array
-    const gameOptions = ["rock", "paper", "scissors"]
-    
-    // Return random array value
-    return (gameOptions[Math.floor((Math.random() * (gameOptions.length )))])
+  // F*ck it, I'm using an array because it's more efficient
+  // Declare array
+  const gameOptions = ["rock", "paper", "scissors"];
+
+  // Return random array value
+  return gameOptions[Math.floor(Math.random() * gameOptions.length)];
 }
 
 // getHumanChoice: collect user input
 function getHumanChoice() {
-    let humanChoice = prompt("Enter a choice: ").toLowerCase()
-    return humanChoice;
+  let humanChoice = prompt("Enter a choice: ").toLowerCase();
+  return humanChoice;
 }
 
 // Global variables for holding scores
-let humanScore = 0
-let computerScore = 0
-
+let humanScore = 0;
+let computerScore = 0;
 
 // playRound: Play a round of the game
 function playRound(playerChoice, computerChoice) {
+  // format the human choice
+  const humanChoice = playerChoice.trim();
+  const computerOption = computerChoice;
 
-    // Ensure player choice matches
-    let ensureMatch = (playerChoice === "rock") || (playerChoice === "paper") || (playerChoice === "scissors")
-    if (!ensureMatch) {
-        console.log("Invalid entry")
-        return
-    }
-    // Simple boolean to check winner
-    let checkResult = (playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")
-    
-    // Compare the values
-    if (checkResult) {
-        humanScore++
-        console.log("Human wins")
-    } else if (!checkResult) {
-        computerScore++
-        console.log(`You lose! ${computerChoice} beats ${playerChoice}`)
-    }
-    else {
-        console.log("It's a tie")
-    }
+  // Compare the human choice, but first make sure the input is correct
+  if (
+    !(
+      humanChoice === "rock" ||
+      humanChoice === "paper" ||
+      humanChoice === "scissors"
+    )
+  ) {
+    console.log("Invalid response!!");
+    return;
+  }
+  // Compare options chosen
+  let wager =
+    (humanChoice === "rock" && computerOption === "scissors") ||
+    (humanChoice === "paper" && computerOption === "rock") ||
+    (humanChoice === "scissors" && computerOption === "paper");
+
+  // Check if Strings are equal
+  if (computerOption === humanChoice) {
+    // Check if tie
+    console.log("It's a tie");
+    return;
+  } else if (wager) {
+    // If the human wins
+    humanScore++;
+    console.log(`Human wins: ${humanChoice} beats ${computerOption}!!!`);
+  } else {
+    computerScore++;
+    console.log(`Computer wins: ${computerOption} beats ${humanChoice}!!!`);
+  }
 }
 
-playRound(getHumanChoice(), getComputerChoice())
